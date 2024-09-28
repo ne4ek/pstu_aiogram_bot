2eae4cf6ba159bc65db2b594cbc5d6b66934497b
ne4ek <nechekpojiloy@gmail.com>
2024-09-28 12:13:55 +0300

    added templates

--- views.py
+++ views.py
@@ -14,36 +14,36 @@ def menu():
 
 @app.route("/menu/naprvlenia")
 def napravlenia():
-    return render_template("napravlenia.html")
+    return render_template("menu-pages/napravlenia.html")
 
 @app.route("/menu/stoimost")
 def stoimost():
-    return render_template("stoimost.html")
+    return render_template("menu-pages/stoimost.html")
 
 @app.route("/menu/obshejitie")
 def obshejitie():
-    return render_template("obshejitie.html")
+    return render_template("menu-pages/obshejitie.html")
 
 @app.route("/menu/contacti")
 def contacti():
-    return render_template("contacti.html")
+    return render_template("menu-pages/contacti.html")
 
 @app.route("/menu/anketi")
 def anketi():
-    return render_template("anketi.html")
+    return render_template("menu-pages/anketi.html")
 
 @app.route("/menu/eksameni")
 def eksameni():
-    return render_template("eksameni.html")
+    return render_template("menu-pages/eksameni.html")
 
 @app.route("/menu/postuplenie")
 def postuplenie():
-    return render_template("postuplenie.html")
+    return render_template("menu-pages/postuplenie.html")
 
 @app.route("/menu/ligoti")
 def ligoti():
-    return render_template("ligoti.html")
+    return render_template("menu-pages/ligoti.html")
 
 @app.route("/menu/o_vuse")
 def o_vuse():
-    return render_template("o_vuse.html")
+    return render_template("menu-pages/o_vuse.html")
